22f4cc51b67cd6c8558240e4555f8cfe572f7c8f
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-01-08 13:51:49 -0400

    error charmap

--- compress.py
+++ compress.py
@@ -1,18 +1,25 @@
- # !/usr/bin/python
+# !/usr/bin/python
 # coding=utf-8
 import re
 
 symbols = {
 #   key             :   symbols[key]
-    "implementation":   "🤯",
-    "practicality"  :   '🤩',
-    "better"        :   '😅',
+    "implementation":   "😫",
+    "practicality"  :   '😐',
+    "better"        :   '🤔',
     "than"          :   '😘',
     "Although"      :   "🥺",
 }
 
 def compress(content):
-
-    compressed_content = ''    
-
-    return content
+    """compress recibe una variable del tipo string que viene
+    del archivo app.py, ya con el texto cargado del document.txt,
+    a continuación se lee la cadena de texto y se reemplaza con cada
+    valor del diccionario
+    """
+    for key, symbol in symbols.items():
+        #content=content.replace(key,symbol)
+        compressed_content = content.replace(key,symbol)
+    print(compressed_content)
+   
+    return compressed_content
--- decompress.py
+++ decompress.py
@@ -6,6 +6,10 @@ from compress import symbols
 def decompress(compressed_content):
 
     decompressed_content = '' 
-
+    for key, symbol in symbols.items():
+        #content=content.replace(key,symbol)
+        decompressed_content = compressed_content.replace(symbol,key)
+    print(decompressed_content)
+    
     return compressed_content
     
